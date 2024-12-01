07c1d5d7b40fe3ce6f2730922b9894898745d951
Stephen Webb <swebb2066@gmail.com>
2024-12-01 12:08:27 +1100

    Improve instructions for building on Windows

--- 1.3.1/build.js
+++ 1.3.1/build.js
@@ -1,25 +1,25 @@
 var build =
 [
     [ "Log4cxx Dependencies", "dependencies.html", [
-      [ "APR+APR-Util", "dependencies.html#autotoc_md8", null ],
-      [ "sed+gzip+zip", "dependencies.html#autotoc_md9", null ],
-      [ "Optional Dependencies", "dependencies.html#autotoc_md10", [
-        [ "A note on C++ version and Boost", "dependencies.html#autotoc_md11", null ]
+      [ "APR+APR-Util", "dependencies.html#autotoc_md2", null ],
+      [ "sed+gzip+zip", "dependencies.html#autotoc_md3", null ],
+      [ "Optional Dependencies", "dependencies.html#autotoc_md4", [
+        [ "A note on C++ version and Boost", "dependencies.html#autotoc_md5", null ]
       ] ],
-      [ "Licenses(direct dependencies only)", "dependencies.html#autotoc_md12", null ]
+      [ "Licenses(direct dependencies only)", "dependencies.html#autotoc_md6", null ]
     ] ],
     [ "Building with CMake", "build-cmake.html", [
-      [ "Supported options", "build-cmake.html#autotoc_md0", null ],
-      [ "Examples", "build-cmake.html#autotoc_md1", [
-        [ "Unix type systems", "build-cmake.html#autotoc_md2", null ],
-        [ "Windows", "build-cmake.html#autotoc_md3", null ],
-        [ "Mac OS/X:", "build-cmake.html#autotoc_md4", null ],
-        [ "FreeBSD:", "build-cmake.html#autotoc_md5", null ]
+      [ "Supported options", "build-cmake.html#autotoc_md18", null ],
+      [ "Examples", "build-cmake.html#autotoc_md19", [
+        [ "Unix type systems", "build-cmake.html#Linux", null ],
+        [ "Windows", "build-cmake.html#Windows", null ],
+        [ "Mac OS/X", "build-cmake.html#MacOS", null ],
+        [ "FreeBSD", "build-cmake.html#FreeBSD", null ]
       ] ]
     ] ],
     [ "Building with vcpkg", "build-vcpkg.html", [
-      [ "Preparation", "build-vcpkg.html#autotoc_md6", null ],
-      [ "Building log4cxx.dll", "build-vcpkg.html#autotoc_md7", null ]
+      [ "Preparation", "build-vcpkg.html#autotoc_md0", null ],
+      [ "Building log4cxx.dll", "build-vcpkg.html#autotoc_md1", null ]
     ] ],
     [ "Testing coverage", "supported-systems.html", null ]
 ];
